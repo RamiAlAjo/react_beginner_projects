2099ac84d87c1e5c193559cbf971e000a98bc2c4
RamiAlAjo <ramialajo@outlook.com>
2025-10-30 22:25:24 +0300

    Adding Calculator Project

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Counter from '../src/Counter/App.jsx'
 import Todo from './Todo/Todo.jsx'
+import Calculator from './Calculator/Calculator.jsx'
 
 createRoot(document.getElementById('root')).render(
 
     // <App />,
     // <Counter />,
-    <Todo />
+    // <Todo />,
+    <Calculator />
 
 )
